2a88fec9699f8ab8521a143e7d47a281ec415a9f
Clark McEvoy <mcevoyclark@gmail.com>
2023-09-14 14:51:33 +0200

    grid + sign logics

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -5,25 +5,33 @@ import React, { useState } from 'react'
 type CardProps = {
     position: number[]
     played: boolean;
-    player: 'X' | 'O' | null;
+    signPlayer: 'X' | 'O' | null;
+    turn: number
+    setTurn: React.Dispatch<React.SetStateAction<number>>
 }
 
 type Sign = {
     sign: 'X' | 'O' | null
 }
 
-export const Card = ({position, played, player}: CardProps ) => {
+export const Card = ({position, played, signPlayer, turn ,setTurn }: CardProps ) => {
     const [sign, setSign] = useState<Sign>({sign: null})
+    const [cardPlayed, setCardPlayed] = useState(false)
+
 
     function handlePlay(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-        setSign({sign: 'X'})
+        setTurn((prev) => prev + 1)
+
+        const signToBePlayed = turn % 2 === 0 ? 'X' : 'O'
+
+        setSign({sign: signToBePlayed})
+        setCardPlayed(true)
     }
 
-
   return (
-    <button onClick={handlePlay} className=" w-44 h-44 rounded-lg m-1 bg-gray-800 hover:bg-gray-700 duration-150 transition-all">
+    <button onClick={handlePlay} className={`w-44 h-44 rounded-lg m-1 bg-gray-800 hover:bg-gray-700 duration-150 transition-all ${cardPlayed && 'bg-gray-700'}`}>
         {/* {position} */}
-        {player}
+        {/* {signPlayer} */}
         {sign.sign}
     </button>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 import Image from 'next/image';
 import Card from './components/Card';
+import { useState } from 'react';
 
 export default function Home() {
+  const [turn, setTurn] = useState(1)
+
   const grid = [
     [0, 0, 0],
     [0, 0, 0],
@@ -11,11 +16,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-900 ">
       <div className="text-white text-xl">My Tic-Tac-Toe</div>
+      <div>Turn: {turn}</div>
       <div>
         {grid.map((row, rowIndex) => (
           <div key={1} className="flex">
             {grid.map((col, colIndex) => (
-              <Card position={[rowIndex, colIndex]} player={null} played={true} key={String(col) + String(colIndex)} />
+              <Card position={[rowIndex, colIndex]} signPlayer={null} played={true} key={String(col) + String(colIndex)} turn={turn} setTurn={setTurn} />
             ))}
           </div>
         ))}
